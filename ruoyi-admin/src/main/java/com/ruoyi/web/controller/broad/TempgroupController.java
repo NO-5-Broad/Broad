07528ebf31d98784fc4c9277d3d2421350e638eb
lenghenglong <1447080303@qq.com>
2019-12-21 20:44:19 +0800

    完善特殊分组管理的申请记录功能

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/broad/TempgroupController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/broad/TempgroupController.java
@@ -18,6 +18,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -135,6 +137,15 @@ public class TempgroupController<addtemp> extends BaseController{
 //        int i = iTempgroupService.insertTempgroup(tempgroup);
 //        return "success";
 //    }
+@PostMapping("/addtemp")
+@ResponseBody
+public String addTemp(Tempgroup tempgroup){
+    Date d = new Date();
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    tempgroup.setCreatetime(sdf.format(d));
+    int i = iTempgroupService.insertTempgroup(tempgroup);
+    return "success";
+}
 
     @PostMapping("/tempgroup_ter")
     @ResponseBody
